e9c2c3f576e7985fd0b6f743e8f25a1230c73fb2
Manikanta <orugantimanikanta24@gmail.com>
2026-01-21 11:56:25 +0530

    redeploying fe

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import os
 from typing import Optional
-from backend.adi_pipeline import process_data, OUTPUT_DIR
+from adi_pipeline import process_data, OUTPUT_DIR
 
 app = FastAPI()
 
@@ -13,7 +13,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5000",
         "http://localhost:5173",
-        "https://uh-git-main-manis-projects-4c168198.vercel.app"
+        "https://uh-git-main-manis-projects-4c168198.vercel.app",
+        "https://gleaming-douhua-b0837a.netlify.app/",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
